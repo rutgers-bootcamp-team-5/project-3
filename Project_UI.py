c07fb2238dba42cedc19983ea49cb5a2552f96c8
sbalamurugen03 <sbalamurugen@yahoo.com>
2024-04-04 23:10:37 -0400

    Latest changes

--- Project_UI.py
+++ Project_UI.py
@@ -7,7 +7,7 @@ from tkinter import filedialog
 
 # initialize Tkinter which manages the GUI elements
 root = Tk()
-root.geometry("600x400")
+root.geometry("600x450")
 root.title('Project 3')
 # Gets the requested values of the height and width.
 winWidth = root.winfo_reqwidth()
@@ -23,6 +23,7 @@ root.geometry("+{}+{}".format(posRight, posDown))
 def Run():
 
     print(f1entry.get())
+    print(ExecuterEntry.get())
 
     if  not f1entry.get():
         err = Toplevel(root)
@@ -39,10 +40,12 @@ def Run():
 
 File1Path = StringVar()
 File2Path = StringVar()
+File3Path = StringVar()
+File4Path = StringVar()
 
 
 def open_file1():
-    global temp1
+    global fpath1
     file_path = filedialog.askopenfilename(initialdir = "C:\Data",
                                           title = "Select a File",
                                           filetypes = (("CSV files",
@@ -50,12 +53,12 @@ def open_file1():
                                                        ("All Files",
                                                         "*.*")))
     if file_path is not None:
-        temp1 = file_path
-        File1Path.set(temp1)
-        return temp1
+        fpath1 = file_path
+        File1Path.set(fpath1)
+        return fpath1
     
 def open_file2():
-    global temp2
+    global fpath2
     file_path = filedialog.askopenfilename(initialdir = "C:\Data",
                                           title = "Select a File",
                                           filetypes = (("CSV files",
@@ -63,9 +66,35 @@ def open_file2():
                                                        ("All Files",
                                                         "*.*")))
     if file_path is not None:
-        temp2 = file_path
-        File2Path.set(temp2)
-        return temp2
+        fpath2 = file_path
+        File2Path.set(fpath2)
+        return fpath2
+    
+def open_file3():
+    global fpath3
+    file_path = filedialog.askopenfilename(initialdir = "C:\Data",
+                                          title = "Select a File",
+                                          filetypes = (("CSV files",
+                                                        "*.csv*"),
+                                                       ("All Files",
+                                                        "*.*")))
+    if file_path is not None:
+        fpath3 = file_path
+        File3Path.set(fpath3)
+        return fpath3
+    
+def open_file4():
+    global fpath4
+    file_path = filedialog.askopenfilename(initialdir = "C:\Data",
+                                          title = "Select a File",
+                                          filetypes = (("CSV files",
+                                                        "*.csv*"),
+                                                       ("All Files",
+                                                        "*.*")))
+    if file_path is not None:
+        fpath4 = file_path
+        File4Path.set(fpath4)
+        return fpath4
 
 frame1 = Frame()
 frame1.pack(fill=X)
@@ -79,12 +108,16 @@ frame5 = Frame()
 frame5.pack(fill=X)
 frame6 = Frame()
 frame6.pack(fill=X)
+frame7 = Frame()
+frame7.pack(fill=X)
+frame8 = Frame()
+frame8.pack(fill=X)
 
 biglabel = Label(frame1, text='Team 5 Project', font=('Arial', 20), fg='blue')
 biglabel.pack(side=TOP)
 
 f1button = Button(frame2, text='Choose File 1', height=2, width=10, command=open_file1)
-label_file_explorer = Label(frame3, text='')
+label_file_explorer = Label(frame2, text='')
 f1entry = Entry(frame2, textvariable=File1Path, state=DISABLED, fg='black')
 f1button.pack(side=LEFT, padx=10, pady=5)
 f1entry.pack(fill=X, pady=20, padx=10)
@@ -95,21 +128,34 @@ f2entry = Entry(frame3, textvariable=File2Path, state=DISABLED, fg='black')
 f2button.pack(side=LEFT, padx=10, pady=5)
 f2entry.pack(fill=X, pady=20, padx=10)
 
+f3button = Button(frame4, text='Choose File 3', height=2, width=10, command=open_file3)
+label_file_explorer = Label(frame4, text='')
+f3entry = Entry(frame4, textvariable=File3Path, state=DISABLED, fg='black')
+f3button.pack(side=LEFT, padx=10, pady=5)
+f3entry.pack(fill=X, pady=20, padx=10)
+
+f4button = Button(frame5, text='Choose File 4', height=2, width=10, command=open_file4)
+label_file_explorer = Label(frame5, text='')
+f4entry = Entry(frame5, textvariable=File4Path, state=DISABLED, fg='black')
+f4button.pack(side=LEFT, padx=10, pady=5)
+f4entry.pack(fill=X, pady=20, padx=10)
+
 # Round Options to choose from
-ExecuterEntry = Entry(frame4, text='0', width=20)
-ExecuterEntryLabel = Label(frame4, text='Executer Name ', font=('Arial', 12))
+ExecuterEntry = Entry(frame6, text='0', width=20)
+ExecuterEntryLabel = Label(frame6, text='Executer Name ', font=('Arial', 12))
 ExecuterEntryLabel.pack(side=LEFT, pady=5, padx=10)
 ExecuterEntry.pack(side=LEFT, padx=15, pady=10)
 
-CommentEntryLabel = Label(frame5, text='Enter Comments', font=('Arial', 12))
+CommentEntryLabel = Label(frame7, text='Enter Comments', font=('Arial', 12))
 CommentEntryLabel.pack(side=LEFT, padx=10)
-CommentEntry = Entry(frame5, text='', width=20)
+CommentEntry = Entry(frame7, text='', width=20)
 CommentEntry.pack(side=LEFT, padx=10, pady=10)
 
-button = Button(frame6, text="Run", height=2, width=10, command=Run)
+button = Button(frame8, text="Run", height=2, width=10, command=Run)
 button.pack(side=BOTTOM, pady=30)
 
 def ETLProcess():
+    print(fpath1)
     finished()
 
 # Handles Popup once application is Finished
